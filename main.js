4fdcb165331f7ded373ceb1ce2923394ceaa413c
madhurasekar9 <sekam810@newschool.edu>
2023-03-01 01:49:46 -0500

    aa

--- main.js
+++ main.js
@@ -11,8 +11,31 @@ imageBlocks.forEach((block) => {
         block.classList.remove('active')
     } 
 })
-	// Put your JavaScript in here!
+var spanText = function spanText(text) {
+    var string = text.innerText;
+    var spaned = '';
+    for (var i = 0; i < string.length; i++) {
+      if(string.substring(i, i + 1) === ' ') spaned += string.substring(i, i + 1);
+      else spaned += '<span>' + string.substring(i, i + 1) + '</span>';
+    }
+    text.innerHTML = spaned;
+  }
+  
+  var headline = document.querySelector("h1");
+  
+  spanText(headline);
+  
+  let animations = document.querySelectorAll('.animation');
+  
+  animations.forEach(animation => {
+    let letters = animation.querySelectorAll('span');
+    letters.forEach((letter, i) => {
+      letter.style.animationDelay = (i * 0.1) + 's';
+    })
+  })
+  // Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
 }
+
 
 
